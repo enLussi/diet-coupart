9101a0e0d0bf120b48689486ac0f528630b83e11
Lucas Martignon <lucas.martignon@wanadoo.fr>
2023-04-23 12:23:25 +0200

    Fixtures admin group

--- src/DataFixtures/AdminFixtures.php
+++ src/DataFixtures/AdminFixtures.php
@@ -4,19 +4,24 @@ namespace App\DataFixtures;
 
 use App\Entity\Administrator;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
-class AdminFixtures extends Fixture
+class AdminFixtures extends Fixture implements FixtureGroupInterface
 {
     public function __construct(private UserPasswordHasherInterface $passwordEncoder) {}
 
     public function load(ObjectManager $manager): void
     {
+
+        //Modifier les valeurs pour générer un nouvel admin
+        //Puis lancer la fixture avec
+        //symfony console doctrine:fixtures:load --group=admin --append
         $admin = new Administrator();
-        $admin->setEmail('sandrine-diet@coupart.fr');
-        $admin->setLastname('Coupart');
-        $admin->setFirstname('Sandrine');
+        $admin->setEmail('admin@diet.fr');
+        $admin->setLastname('Admin');
+        $admin->setFirstname('Admin');
         $admin->setPassword(
             $this->passwordEncoder->hashPassword($admin, 'admin')
         );
@@ -25,5 +30,10 @@ class AdminFixtures extends Fixture
         $manager->persist($admin);
 
         $manager->flush();
+    }
+
+    public static function getGroups(): array
+    {
+        return ['admin'];
     }
 }
