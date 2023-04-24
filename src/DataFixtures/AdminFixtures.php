47e9b1670b261851c98d83852b56205d8256f49b
Lucas Martignon <lucas.martignon@wanadoo.fr>
2023-04-24 09:10:52 +0200

    Add newADmin.json and modify methods in Admin Fixtures

--- src/DataFixtures/AdminFixtures.php
+++ src/DataFixtures/AdminFixtures.php
@@ -15,15 +15,18 @@ class AdminFixtures extends Fixture implements FixtureGroupInterface
     public function load(ObjectManager $manager): void
     {
 
-        //Modifier les valeurs pour générer un nouvel admin
+        //Modifier les valeurs pour générer un nouvel admin dans newAdmin.json 
         //Puis lancer la fixture avec
         //symfony console doctrine:fixtures:load --group=admin --append
+
+        $params = json_decode(file_get_contents('newAdmin.json'), true);
+
         $admin = new Administrator();
-        $admin->setEmail('admin@diet.fr');
-        $admin->setLastname('Admin');
-        $admin->setFirstname('Admin');
+        $admin->setEmail($params['email']);
+        $admin->setLastname($params['lastname']);
+        $admin->setFirstname($params['firstname']);
         $admin->setPassword(
-            $this->passwordEncoder->hashPassword($admin, 'admin')
+            $this->passwordEncoder->hashPassword($admin, $params['password'])
         );
         $admin->setRoles(['ROLE_ADMIN']);
 
